a8e428364c08538010245e8440b53dc74f7e552f
Rafael <rpclerici@gmail.com>
2023-06-12 14:54:57 -0300

    RollingTank
    Created a "Rolling Tank" made using multiple rotating mesh colliders to propel it forwards.

---
+++ Assets/Scripts/RollingTankController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingTankController : MonoBehaviour
+{
+    public List<Rigidbody> LeftWheelies;
+    public List<Rigidbody> RightWheelies;
+
+    public float torqueForward;
+    public float torqueBackward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)){
+            foreach (Rigidbody wheelie in LeftWheelies){
+                wheelie.maxAngularVelocity = 10;
+                wheelie.AddTorque(Vector3.right * torqueForward);
+            }
+        }
+        if(Input.GetKey(KeyCode.S)){
+            foreach (Rigidbody wheelie in LeftWheelies){
+                wheelie.maxAngularVelocity = 10;
+                wheelie.AddTorque(Vector3.right * torqueBackward);
+            }
+        }
+        if(Input.GetKey(KeyCode.E)){
+            foreach (Rigidbody wheelie in RightWheelies){
+                wheelie.maxAngularVelocity = 10;
+                wheelie.AddTorque(Vector3.right * torqueForward);
+            }
+        }
+        if(Input.GetKey(KeyCode.D)){
+            foreach (Rigidbody wheelie in RightWheelies){
+                wheelie.maxAngularVelocity = 10;
+                wheelie.AddTorque(Vector3.right * torqueBackward);
+            }
+        }
+
+        if(Input.GetKey(KeyCode.W) == false && Input.GetKey(KeyCode.S)== false){
+            foreach (Rigidbody wheelie in LeftWheelies){
+                wheelie.maxAngularVelocity = 0;
+            }
+        }
+        if(Input.GetKey(KeyCode.E) == false && Input.GetKey(KeyCode.D)== false){
+            foreach (Rigidbody wheelie in RightWheelies){
+                wheelie.maxAngularVelocity = 0;
+            }
+        }
+    }
+}
